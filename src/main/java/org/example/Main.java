6ab22c52fa10b1c42e0f5b37d9021558e22c20cb
Dexter <gusevsergey85@gmail.com>
2022-12-16 10:33:05 +0300

    Добавил задание

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
+package org.example;
+/*Прошу вас написать приложение,
+        которое хранит информацию о фильмах
+        и позволяет получaть все детали о них.
+        +реализовать поиск по части имени фильма
+*/
+public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
